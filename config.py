300b84025b61c244e8b1f7eb3395789cec4233c5
acgreene <acgreene@umich.edu>
2021-06-21 11:57:44 -0500

    v1.01- TDA Interfacing
    Starting using the tda api to begin interfacing the algotrader with tda.

--- config.py
+++ config.py
@@ -3,6 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv() # take environment variables from .env.
 
+#webdriver to open Firefox with selenium
+geckodriver_path='/Users/Alec Greene/Desktop/Alec/CODING/algotrader/geckodriver'
+
 # Alpaca 
 APCA_BASE_URL = "https://paper-api.alpaca.markets"
 APCA_ACCOUNT_URL = "{}/v2/account".format(APCA_BASE_URL)
@@ -15,4 +18,4 @@ APCA_HEADERS = {'APCA-API-KEY-ID': os.getenv('APCA_API_KEY'),
 TDA_API_KEY = os.getenv('TDA_API_KEY')
 TDA_ACCOUNT_ID = os.getenv('TDA_ACCOUNT_ID')
 TDA_TOKEN_PATH = 'token'
-TDA_REDIRECT_URL = 'https://localhost'
+TDA_REDIRECT_URL = 'https://localhost' #set here since we are running algo on our local computer
